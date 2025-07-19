f2054b4c589353971d801fe8562770cd8c7cd753
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2025-07-18 23:20:00 -0300

    MathController With Algorithm

--- course/src/main/java/com/curso_erudio/course/controllers/MathController.java
+++ course/src/main/java/com/curso_erudio/course/controllers/MathController.java
@@ -9,9 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
 	@RequestMapping("/sum/{numberOne}/{numberTwo}")
-	public Double sum(@PathVariable("numberOne") String numberOne, 
-			        @PathVariable("numberTwo") String numeberTwo) {
-		return 1D;
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
+			throws Exception {
+
+		if (!isNumeric(numberOne, numberTwo))
+			throw new Exception("Error você está iinserindo caracters");
+
+		Double number1 = Double.parseDouble(numberOne);
+		Double number2 = Double.parseDouble(numberTwo);
+
+		return number1 + number2;
+	}
+
+	private boolean isNumeric(String numberOne, String numberTwo) {
+		if (numberOne == null && numberTwo == null || numberOne.isEmpty() && numberTwo.isEmpty())
+			return false;
+		if (numberOne.matches("[-+]?[0-9]*\\.?[0-9]+") && numberTwo.matches("[-+]?[0-9]*\\.?[0-9]+")) {
+			return true;
+		}
+		return false;
+
 	}
 }
-
