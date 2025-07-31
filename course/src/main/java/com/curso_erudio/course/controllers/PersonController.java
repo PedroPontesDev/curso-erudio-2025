c856ce06802c90cffda921a5fc252fb3efdd65bb
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2025-07-31 15:22:49 -0300

    Usando Loggin Level no Yml  a Nivel de Pacote e Debug

--- course/src/main/java/com/curso_erudio/course/controllers/PersonController.java
+++ course/src/main/java/com/curso_erudio/course/controllers/PersonController.java
@@ -1,11 +1,17 @@
 package com.curso_erudio.course.controllers;
 
-import java.awt.PageAttributes.MediaType;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +25,35 @@ public class PersonController {
 	@Autowired
 	PersonServices personServices;
 	
-	@RequestMapping(path = "/{id}", consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/{id}")
 	public ResponseEntity<Person> findById(@PathVariable Long id) {
 		Person finded = personServices.findById(id);
 		return new ResponseEntity<>(finded, HttpStatus.OK);
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<Person>> findAll() {
+		List<Person> persons = personServices.findAll();
+		return new ResponseEntity<>(persons, HttpStatus.OK);
+	}
+	
+	@PostMapping(path = "/create")
+	public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+		var newPerson = personServices.savePerson(person);
+		return new ResponseEntity<>(newPerson, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(path = "/update")
+	public ResponseEntity<Person> updatePerson(@RequestBody Person person) {
+		var updated = personServices.update(person);
+		return new ResponseEntity<>(updated, HttpStatus.OK);
+	}
+	
 
+	@DeleteMapping(path = "/delete/{id}" )
+	public ResponseEntity<?> deletePerson(@PathVariable Long id) {
+		personServices.delePerson(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- course/src/main/java/com/curso_erudio/course/services/PersonServices.java
+++ course/src/main/java/com/curso_erudio/course/services/PersonServices.java
@@ -16,16 +16,15 @@ import com.curso_erudio.course.repository.PersonRepository;
 @Service
 public class PersonServices {
 
-	private AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
 	@Autowired
 	private PersonRepository personRepo;
 
 	public Person findById(Long Id) {
-		logger.info("Finding Person one Person DataBase");
+		logger.info("Finding Persons");
 		Person existentPerson = personRepo.findById(Id)
-								.orElseThrow(() -> new ResourceNotFoundException("RESOURCE NOT FOUND WITH THIS ID!"));
+								.orElseThrow(() -> new ResourceNotFoundException("RESOURCE NOT FOUND WITH THIS ID!" + Id));
 		return existentPerson;	
 	}
 
@@ -34,17 +33,35 @@ public class PersonServices {
 		
 		var persons = personRepo.findAll();
 		
-		if(persons.isEmpty()) throw new ResourceNotFoundException("NOTHING FOUND IN THIS LITS BITCH");
+		if(persons.isEmpty()) throw new ResourceNotFoundException("RESOURCES NOT FOUND");
 		
 		return persons;
 	}
 
+	public Person savePerson(Person newPerson) {
+		logger.info("Saving new Person");
+		if(newPerson != null) {
+			return personRepo.save(newPerson);
+		} throw new ResourceNotFoundException("Its not possible save this person in database");
+	}
 	
+	public void delePerson(Long id) {
+		logger.info("Deleting Person");
+		Person existent = personRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found for this ID"));
+		personRepo.delete(existent);
+		
+	}
 
-	public Person createPerson(Person newPerson) throws Exception {
-		if (newPerson == null)
-			throw new Exception("Pessoa está nula");
-		return personRepo.save(newPerson);
+	public Person update(Person person) {
+		  logger.info("Updating one Person!");
+	      Person entity = personRepo.findById(person.getId())
+	                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	      entity.setAddres(person.getAddres());
+	      entity.setFirstName(person.getFirstName());
+	      entity.setLastName(person.getLastName());
+	      entity.setGender(person.getGender());
+	      personRepo.save(entity);
+	      return entity;
 	}
 
 }
